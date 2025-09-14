129f1bf8295d62c4b2c81ebf35ced7fb722640e8
Sumonsky86 <seba_21@outlook.com.ar>
2025-09-14 16:43:04 -0300

    fix stats vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
+import path from 'path';
 import sitemap from '@astrojs/sitemap';
-
 
 const site = process.env.PUBLIC_SITE_URL || 'http://localhost:4321';
 
 export default defineConfig({
   site,
   vite: {
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@data': path.resolve('./src/data'),
+        '@assets': path.resolve('./src/assets'),
+        '@styles': path.resolve('./src/styles'),
+        '@utils': path.resolve('./src/utils'),
+      }
+    },
     plugins: [tailwindcss()],
     css: {
       preprocessorOptions: {
@@ -42,12 +51,8 @@ export default defineConfig({
   ],
   image: {
     remotePatterns: [
-      {
-        protocol: "https"
-      },
-      {
-        protocol: "http"
-      }
+      { protocol: "https" },
+      { protocol: "http" }
     ]
   }
 });
